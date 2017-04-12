2a828b7b4caab0eed7edc992053291cf2998f39e
kangfei <kang_chao520@163.com>
2017-04-12 14:52:18 +0800

    Update tools.cpp
    modify changes tootls

--- src/tools.cpp
+++ src/tools.cpp
@@ -14,28 +14,24 @@ VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
     VectorXd rmse(4);
     rmse << 0,0,0,0;
 
-    // check the validity of the following inputs:
-    //  * the estimation vector size should not be zero
+
     assert(estimations.size()>0);
 
     //  * the estimation vector size should equal ground truth vector size
     assert(estimations.size() == ground_truth.size());
 
-    //accumulate squared residuals
-    VectorXd squared_residuals(estimations.size());
-    for(int i=0; i < estimations.size(); ++i){
-        // ... your code here
-        VectorXd residual = estimations[i]-ground_truth[i];
 
+    for(int i=0; i < estimations.size(); ++i){
+        VectorXd residual = estimations[i]-ground_truth[i];
         squared_residuals = residual.array()*residual.array();
         rmse += squared_residuals;
     }
 
     //calculate the mean
-    VectorXd mean = rmse/estimations.size();
+    rmse = rmse/estimations.size();
 
     //calculate the squared root
-    rmse = mean.array().sqrt();
+    rmse = rmse.array().sqrt();
 
     //return the result
     return rmse;
@@ -51,33 +47,25 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
     float vy = x_state(3);
 
     //TODO: YOUR CODE HERE
-    double c1 = px*px + py*py;
-    double c2 = sqrt(c1);
+    double x1 = px*px + py*py;
+    double x2 = sqrt(x1);
+    double x3 =x1*x2
     //check division by zero
-    if (c1<0.0001)
-    {
+    if (c1<0.0001){
         std::cout << "Division by zero ERROR!" << std::endl;
         return Hj;
     }
     //compute the Jacobian matrix
-    double dp_dpx = px/c2;
-    double dp_dpy = py/c2;
-    double dp_dvx = 0;
-    double dp_dvy = 0;
-
-    double dphi_dpx = -py/c1;
-    double dphi_dpy = px/c1;
-    double dphi_dvx = 0;
-    double dphi_dvy = 0;
-
-    double dpdot_dpx = py*(vx*py - vy*px) / pow(c2,3);
-    double dpdot_dpy = px*(vy*px - vx*py) / pow(c2,3);
-    double dpdot_dvx = dp_dpx;
-    double dpdot_dvy = dp_dpy;
-
-    Hj <<   dp_dpx, dp_dpy, dp_dvx, dp_dvy,
-            dphi_dpx, dphi_dpy, dphi_dvx, dphi_dvy,
-            dpdot_dpx, dpdot_dpy, dpdot_dvx, dpdot_dvy;
+    double px_x2=px/x2;
+    double py_x2 = py/x2;
+    double py_x1=-py/x1;
+    double px_x1 = px/x1;
+    double temp1 =py*(vx*py - vy*px)/x3;
+    double temp2 = px*(px*vy - py*vx)/x3;
+    Hj << px_x2, py_x2, 0, 0,
+            py_x1, px_x1, 0, 0,
+            temp1, temp2 , px_x2, py_x2;
+   
 
     return Hj;
 }
