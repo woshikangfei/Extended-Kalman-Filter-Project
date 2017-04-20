239d1ac42f203eade4c18ee10943b058d29827e5
kangfei <kang_chao520@163.com>
2017-04-20 14:38:46 +0800

    Update tools.cpp
    modify compile errors

--- src/tools.cpp
+++ src/tools.cpp
@@ -48,7 +48,7 @@ MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
 
     double x1 = px*px + py*py;
     double x2 = sqrt(x1);
-    double x3 =x1*x2
+    double x3 =x1*x2;
     //check division by zero
     if (c1<0.0001){
         std::cout << "Division by zero ERROR!" << std::endl;
